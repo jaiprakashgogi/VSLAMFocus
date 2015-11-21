995efdb5626935f02c43b2760dc49b34e07879f2
Jai Prakash <jaiprakashgogi@gmail.com>
2015-11-20 20:13:50 -0500

    [VSLAM] Added PTAM library for Android. Camera working but no preview. Something to do with the memory allocation.

---
+++ PTAMActivity/jni/Application.mk
@@ -0,0 +1,5 @@
+APP_STL := gnustl_static
+#APP_STL := stlport_static 
+APP_CPPFLAGS := -frtti -fexceptions -std=c++0x -mfloat-abi=softfp -mfpu=neon -Wno-deprecated
+APP_ABI := armeabi
+APP_OPTIM := debug
